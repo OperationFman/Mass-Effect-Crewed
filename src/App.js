2b75abbe7d8fcba7f5ddd9a0df69484518bd3518
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-18 15:17:10 +1100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AuthenticationButton from "./components/authentication-button";
 import Profile from "./components/show-name-test";
 
 function App() {
+  
+  
   return (
     <div className="App">
       <header className="App-header">
--- src/components/show-name-test.jsx
+++ src/components/show-name-test.jsx
@@ -8,6 +8,8 @@ const Profile = () => {
     return <div>Loading ...</div>;
   }
 
+  console.log('User Authentication: ' + isAuthenticated);
+
   return (
     isAuthenticated && (
       <div>
