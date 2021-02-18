c4829c848a4835590beb46e3cd3d7987116bd703
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-18 14:17:12 +1100

    Show user information inapp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AuthenticationButton from "./components/authentication-button";
+import Profile from "./components/show-name-test";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <AuthenticationButton />
+        <Profile />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/authentication-button.jsx
+++ src/components/authentication-button.jsx
@@ -5,7 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const AuthenticationButton = () => {
   const { isAuthenticated } = useAuth0();
-
+  
   return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 };
 
