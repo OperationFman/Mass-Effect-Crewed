6ba4e2c2149235dbe99cfa0d44f5b34e2e2f00a8
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-19 12:31:31 +1100

    Logout button has soem css to seperate it from others so it's lower down the dom

--- src/components/logout-button.jsx
+++ src/components/logout-button.jsx
@@ -1,12 +1,11 @@
-  
-import React from "react";
+  import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
     <button
-      className="btn btn-outline-danger btn-block Main-buttons"
+      className="btn btn-outline-danger btn-block Main-buttons Log-out"
       onClick={() =>
         logout({
           returnTo: window.location.origin,
