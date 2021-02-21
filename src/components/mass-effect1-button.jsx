7aafa7b2aaf7c46432e02de3a14e903640dc60d2
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-21 19:31:42 +1100

    Removed test console.logs

--- src/components/mass-effect1-button.jsx
+++ src/components/mass-effect1-button.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const MassEffect1Button = () => {
-  const test = useAuth0
-  console.log(test);
     return (
     <button
       className="btn btn-primary btn-block mx-auto Main-buttons"
