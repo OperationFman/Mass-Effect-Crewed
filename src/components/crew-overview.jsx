ede7d54f58d54054a3b5884b95922f5ec806cccc
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-21 20:51:58 +1100

    Added simple crew overview component

---
+++ src/components/crew-overview.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Overview extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Hello From Overview</h1>
+        );
+    }
+}
+ 
+export default Overview;
--- src/components/king.jsx
+++ src/components/king.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GameSelect from './game-select'
+import Overview from './crew-overview'
 
 class KingContent extends Component {
     state = {
@@ -15,11 +16,10 @@ class KingContent extends Component {
     };
     
     render() { 
-        console.log(this.state.content)
         const renderContent = () => {
             switch(this.state.content) {
                 case 'MassEffect1':
-                    return <h1>Hello World!</h1>
+                    return <Overview />
                 default:
                     return <GameSelect onLoadMassEffect1Click={this.handleLoadMassEffect1}/>
             };
