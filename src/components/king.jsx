a7ba4eb06adc2e7c82262d14c77cebaf8c45355e
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-21 20:41:47 +1100

    Clicking Mass effect one now updates the page using a switch case function

--- src/components/king.jsx
+++ src/components/king.jsx
@@ -11,16 +11,26 @@ class KingContent extends Component {
     };
 
     handleLoadMassEffect1 = () => {
-        console.log('Mass Effect 1 Clicked');
         this.setState({ content: 'MassEffect1' })
     };
     
     render() { 
         console.log(this.state.content)
-        return (  
-            <GameSelect 
-                onLoadMassEffect1Click={this.handleLoadMassEffect1}
-            />
+        const renderContent = () => {
+            switch(this.state.content) {
+                case 'gameSelect':
+                    return <GameSelect onLoadMassEffect1Click={this.handleLoadMassEffect1}/>
+                case 'MassEffect1':
+                    return <h1>Hello World!</h1>
+                default:
+                    return "It defaulted"
+            };
+        };
+        
+        return (
+            <React.Fragment>
+                {renderContent()}
+            </React.Fragment>
         );
     }
 }
