6d8b1e1d45f8a67b41652e4b788d6025d312b2fe
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-21 20:43:38 +1100

    since GameSelect is the default view when loading the page, it is now the default switch case too

--- src/components/king.jsx
+++ src/components/king.jsx
@@ -18,12 +18,10 @@ class KingContent extends Component {
         console.log(this.state.content)
         const renderContent = () => {
             switch(this.state.content) {
-                case 'gameSelect':
-                    return <GameSelect onLoadMassEffect1Click={this.handleLoadMassEffect1}/>
                 case 'MassEffect1':
                     return <h1>Hello World!</h1>
                 default:
-                    return "It defaulted"
+                    return <GameSelect onLoadMassEffect1Click={this.handleLoadMassEffect1}/>
             };
         };
         
