526dd3f08a8f04175dd22b5d1215e1cbbb452484
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-22 18:12:37 +1100

    Updated some syntax, nothing major

--- src/components/show-crew-list.jsx
+++ src/components/show-crew-list.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 class ShowCrewlist extends Component {
     state = {  }
     render() { 
-        return (  
-            <h1> Showing Crew list</h1>
+        return ( 
+            <React.Fragment>
+            <h3>Crew</h3>
+            <h3>Crew2</h3>
+            <h3>Crew3</h3>
+            </React.Fragment> 
         );
     }
 }
