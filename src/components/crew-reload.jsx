6136b9e6752daa656d2d278d28c0f79428d516e6
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-22 16:34:29 +1100

    Added refresh button and css flexbox for crew view

--- src/components/crew-reload.jsx
+++ src/components/crew-reload.jsx
@@ -4,10 +4,22 @@ class CrewReload extends Component {
     generateTwoCrew() {
         console.log('Generate 2 Cerew Clicked');
     }
-    
+
     render() {  
-        return ( 
-            <h1></h1>
+        return (
+            <div className='crew-reload-container'>
+                <div className='left-crew-options'>
+                    <h2>Crew1</h2>
+                    <div className='crew-divide-line'></div>
+                    <h2>Crew2</h2>
+                </div>
+                <div className="right-refresh">
+                    <button type="button" 
+                        className="btn btn-outline-info"
+                        style={{textAlign:"center", height:"80%", width:"80%", margin:"10% 0% 10% 10%"}}
+                        >🗘</button>
+                </div>
+            </div> 
         );
     }
 }
