ae831bc077f972d65ddf0fc0231a6184cc28c5d6
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-22 15:53:58 +1100

    Added CrewReload component

---
+++ src/components/2-crew-reload.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CrewReload extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <h1>Crew Reload Stuff</h1>
+        );
+    }
+}
+ 
+export default CrewReload;
--- src/components/crew-overview.jsx
+++ src/components/crew-overview.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import CrewReload from './2-crew-reload';
 
 class Overview extends Component {
     state = {  }
     render() {
         const { gameTitle } = this.props;
         return ( 
-            <h1>You've selected: {gameTitle}</h1>
+            <div className="">
+                <CrewReload />
+                <h1>You've selected: {gameTitle}</h1>
+            </div>
         );
     }
 }
