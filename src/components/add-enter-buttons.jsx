52226f5170ac9e69b8ebc43c94b949f071e21b2d
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-22 17:10:23 +1100

    Added + to add-enter buttons and styles

--- src/components/add-enter-buttons.jsx
+++ src/components/add-enter-buttons.jsx
@@ -4,12 +4,18 @@ class AddEnterButtons extends Component {
     state = {  }
     render() { 
         return (  
+            <React.Fragment>
             <button
-                className="btn btn-primary btn-block mx-auto Main-buttons"
+                className="btn btn-primary btn-block mx-auto Add-enter-buttons"
             >
             Confirm
             </button>
-      
+            <button
+                className="btn btn-outline-dark btn-block mx-auto Add-enter-buttons"
+            >
+            +
+            </button>
+            </React.Fragment>
         );
     }
 }
