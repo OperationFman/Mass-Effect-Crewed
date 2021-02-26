aba3a11b6ebe640c310ac3c776c28193549f6c35
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-26 12:28:37 +1100

    Added basic map to iterate through the array of crew names

--- src/components/show-crew-list.jsx
+++ src/components/show-crew-list.jsx
@@ -3,13 +3,16 @@ import CrewItem from './show-crew-item';
 
 class ShowCrewlist extends Component {
     state = {  }
+
     render() {
         const {crew} = this.props;
+
         return ( 
             <React.Fragment>
-            <CrewItem crewmate={crew[0]}/>
-            <CrewItem crewmate={crew[1]}/>
-            <CrewItem crewmate={crew[2]}/>
+            {crew.map((crewmate) => {
+                return <CrewItem crewmate={crewmate}/>
+            })};
+
             <button type="button" style={{marginTop:"10%"}}
                 className="btn btn-outline-dark"
                 onClick={() => this.props.onClickHideCrew()}>Hide Crew
