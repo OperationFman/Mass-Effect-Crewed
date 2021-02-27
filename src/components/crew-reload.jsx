5917ef910427f5cbaa52d06e67f1d0fd9e575460
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-27 15:56:31 +1100

    Update crew-reload.jsx

--- src/components/crew-reload.jsx
+++ src/components/crew-reload.jsx
@@ -8,37 +8,26 @@ class CrewReload extends Component {
     getUnusedCrew = () => {
         const localizeCrewList = this.props.crew;
         const selectLeastFour = localizeCrewList.slice(-4);
-        const randomizer = Math.floor(Math.random() * selectLeastFour.length); 
-        const result = [selectLeastFour[randomizer], selectLeastFour[randomizer - 1]];
-        console.log(selectLeastFour);
-        console.log(result);
-        console.log(randomizer);
+        const randomizer = Math.floor(Math.random() * (selectLeastFour.length - 1) + 1);
+        const result = [selectLeastFour[randomizer], selectLeastFour[randomizer -1]];
+        return result;
     };
 
     render() { 
+        var renderCrewName = this.getUnusedCrew()
         return (
             <div className='crew-reload-container'>
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
-                {this.getUnusedCrew()}
                 <div className='left-crew-options'>
-                    <h2>Tali</h2>
+                    <h2>{renderCrewName[0]}</h2>
                     <div className='crew-divide-line'></div>
-                    <h2>Zaeed</h2>
+                    <h2>{renderCrewName[1]}</h2>
                 </div>
                 <div className="right-refresh">
                     <button type="button" 
                         className="btn btn-outline-info"
                         style={{textAlign:"center", height:"90%", width:"100%", 
                         fontSize:"calc(30px + 2vmin)"}}
+                        onClick={() => this.getUnusedCrew()}
                         >🗘</button>
                 </div>
             </div> 
