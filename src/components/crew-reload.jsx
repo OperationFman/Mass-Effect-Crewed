3cefd683b21f08ab3809d51ea131473afede7ad4
Franklin Moon <franklin.m.moon@gmail.com>
2021-02-28 13:59:58 +1100

    Elevated state for getUnusedCrew, updateCrewstate, handleRefresh

--- src/components/crew-reload.jsx
+++ src/components/crew-reload.jsx
@@ -1,41 +1,17 @@
 import React, { Component } from 'react';
 
 class CrewReload extends Component {
-    state = {
-        shownCrewmate1: '',
-        shownCrewmate2: '',
-    }
-    
-    getUnusedCrew = () => {
-        const localizeCrewList = this.props.crew;
-        const selectLeastFour = localizeCrewList.slice(-4);
-        const randomizer = Math.floor(Math.random() * (selectLeastFour.length - 1) + 1);
-        const result = [selectLeastFour[randomizer], selectLeastFour[randomizer -1]];
-        return result;
-    };
-
-    updateCrewState = () => {
-        var renderCrewNames = this.getUnusedCrew();
-        this.state.shownCrewmate1 = renderCrewNames[0];
-        this.state.shownCrewmate2 = renderCrewNames[1];
-    };
-
-    handleRefresh = () => {
-        this.forceUpdate();
-    };
-
     render() { 
-        this.updateCrewState();
         return (
             <div className='crew-reload-container'>
                 <div className='left-crew-options'>
-                    <h2>{this.state.shownCrewmate1}</h2>
+                    <h2>{this.props.shownCrewmate1}</h2>
                     <div className='crew-divide-line'></div>
-                    <h2>{this.state.shownCrewmate2}</h2>
+                    <h2>{this.props.shownCrewmate2}</h2>
                 </div>
                 <div className="right-refresh">
                 <button type="button" 
-                    onClick={this.handleRefresh}
+                    onClick={this.props.handleRefresh}
                     className="btn btn-outline-info"
                     style={{textAlign:"center", height:"90%", width:"100%", 
                             fontSize:"calc(30px + 2vmin)"}}
--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -5,18 +5,44 @@ import ShowCrew from './show-crew';
 
 class Overview extends Component {
     state = {  
-        crew: ['Ashley', 'Kaiden', 'Liara', 'Wrex', 'Garrus', 'Tali']
+        crew: ['Ashley', 'Kaiden', 'Liara', 'Wrex', 'Garrus', 'Tali'],
+        shownCrewmate1: '',
+        shownCrewmate2: ''
     };
 
     handleSelectedCrew = () => {
         console.log('handling crew from overview');
     };
 
+    getUnusedCrew = () => {
+        const localizeCrewList = this.state.crew;
+        const selectLeastFour = localizeCrewList.slice(-4);
+        const randomizer = Math.floor(Math.random() * (selectLeastFour.length - 1) + 1);
+        const result = [selectLeastFour[randomizer], selectLeastFour[randomizer -1]];
+        return result;
+    };
+
+    updateCrewState = () => {
+        var renderCrewNames = this.getUnusedCrew();
+        this.state.shownCrewmate1 = renderCrewNames[0];
+        this.state.shownCrewmate2 = renderCrewNames[1];
+    };
+
+    handleRefresh = () => {
+        this.forceUpdate();
+    };
+
     render() {
         const { gameTitle } = this.props;
+        this.updateCrewState();
         return ( 
             <div className="overview-container">
-                <CrewReload crew={this.state.crew}/>
+                <CrewReload crew={this.state.crew}
+                            shownCrewmate1={this.state.shownCrewmate1}
+                            shownCrewmate2={this.state.shownCrewmate2}
+                            updateCrewState={this.updateCrewState}
+                            handleRefresh={this.handleRefresh}
+                            />
                 <AddEnter crew={this.state.crew}/>
                 <ShowCrew crew={this.state.crew}/>
             </div>
