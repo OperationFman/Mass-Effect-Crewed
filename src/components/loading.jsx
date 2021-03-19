cb22f89101172db251a9aa138593f0b0b88a3bc6
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-19 14:33:31 +1100

    Create loading.jsx

---
+++ src/components/loading.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Loading
+ extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Loading...</h1> );
+    }
+}
+ 
+export default Loading
+;
