043e98b15489fcf5a43c2ea04bc7e478ba316d2e
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-19 14:58:51 +1100

    Seeing what <4 crew does to the program, apparently nothing

--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -5,7 +5,7 @@ import ShowCrew from './show-crew';
 
 class Overview extends Component {
     state = {  
-        crew: ['Old1', 'Old2', 'Old3', 'Old4', 'Old5', 'Old6'],
+        crew: ['Old4', 'Old5', 'Old6'],
         shownCrewmate1: '',
         shownCrewmate2: ''
     };
