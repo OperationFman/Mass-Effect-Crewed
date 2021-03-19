949e9cf7b7e65147315f2ba559aa617a88d8f5e8
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-19 14:59:02 +1100

    Added loading spinner, prebuilt

---
+++ src/components/Orbitals/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+export default function Orbitals({ color, className, style }) {
+  return (
+    <div className={classNames(styles['lds-orbitals'], className)} style={{ ...style }}>
+      <div className={classNames(styles['center'])} style={{ background: color }}></div>
+      <div className={classNames(styles['inner-spin'])}>
+        <div
+          className={classNames(styles['inner-arc'], styles['inner-arc_start-a'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div
+          className={classNames(styles['inner-arc'], styles['inner-arc_end-a'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div
+          className={classNames(styles['inner-arc'], styles['inner-arc_start-b'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div
+          className={classNames(styles['inner-arc'], styles['inner-arc_end-b'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div className={classNames(styles['inner-moon-a'])} style={{ background: color }}></div>
+        <div className={classNames(styles['inner-moon-b'])} style={{ background: color }}></div>
+      </div>
+      <div className={classNames(styles['outer-spin'])}>
+        <div
+          className={classNames(styles['outer-arc'], styles['outer-arc_start-a'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div
+          className={classNames(styles['outer-arc'], styles['outer-arc_end-a'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div
+          className={classNames(styles['outer-arc'], styles['outer-arc_start-b'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div
+          className={classNames(styles['outer-arc'], styles['outer-arc_end-b'])}
+          style={{ borderColor: color }}
+        ></div>
+        <div className={classNames(styles['outer-moon-a'])} style={{ background: color }}></div>
+        <div className={classNames(styles['outer-moon-b'])} style={{ background: color }}></div>
+      </div>
+    </div>
+  )
+}
+
+Orbitals.propTypes = {
+  /** hex color */
+  color: PropTypes.string,
+  /** class name  */
+  className: PropTypes.string,
+  /** style object */
+  style: PropTypes.object,
+}
+
+Orbitals.defaultProps = {
+  color: '#8c8c8c',
+  className: '',
+  style: {},
+}
