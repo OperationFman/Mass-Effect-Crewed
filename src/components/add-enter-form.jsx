700e82b7c90528d6d2f10e38df07f0f0d9bd4329
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-23 10:29:15 +1100

    Refactored render into a form with submit instead of button

--- src/components/add-enter-form.jsx
+++ src/components/add-enter-form.jsx
@@ -19,18 +19,17 @@ class AddEnterForm extends Component {
   
     render() { 
         return ( 
-            <React.Fragment>
-            <input type="text"
-                   style={{width:"100%", height: "70px", textAlign:"center", border:"1px solid grey"}}
-                   placeholder="Enter Name Here"></input>
-            <button
-                className="btn btn-primary btn-block mx-auto Add-enter-buttons"
-                style={{marginTop:"5%", width:"75%"}}
-                onClick={() => this.onClick()}
-            >
-            Add
-            </button>
-            
+            <form onSubmit={this.onAddClick}>
+            <input  type="text"
+                    value={this.state.formValue}
+                    onChange={this.handleChange}
+                    placeholder='Enter Name Here'
+                    style={{width:"100%", height: "70px", textAlign:"center", border:"1px solid grey"}}
+            ></input>
+            <input  type="submit"
+                    value="Add"
+                    className="btn btn-primary btn-block mx-auto Add-enter-buttons"
+                    style={{marginTop:"5%", width:"75%"}}
             <button
                 className="btn btn-secondary btn-block mx-auto Add-enter-buttons"
                 style={{width:"75%"}}
