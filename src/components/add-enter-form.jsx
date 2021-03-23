469e54f49f44434311f0625a02688260f3f314dc
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-23 15:48:52 +1100

    Refactored how data is received in the form and submitted

--- src/components/add-enter-form.jsx
+++ src/components/add-enter-form.jsx
@@ -7,37 +7,33 @@ class AddEnterForm extends React.Component {
     }
   
     handleSubmit(event) {
-
-    render() { 
-        return ( 
-            <div className="">
-                <form onSubmit={this.handleSubmit}>
-                    <input  type="text"
-                            placeholder='Enter Name Here'
-                            style={{width:"100%", height: "70px", textAlign:"center", border:"1px solid grey"}}
-                    ></input>
-                    <button  type="button"
-                            className="btn btn-primary btn-block mx-auto Add-enter-buttons"
-                            style={{marginTop:"5%", width:"75%"}}
-                    >
-                    Add
-                    </button>
-                </form> 
       event.preventDefault();
       const data = new FormData(event.target);
       this.props.addCrew(data.get('newName'));
       this.props.onAddCrewmate() //This actually closes it, Refactor to say so
     }
+  
+    render() {
+      return (
+        <React.Fragment>
+          <form onSubmit={this.handleSubmit}>
+            <input  type="text" 
+                    name="newName"
+                    placeholder='Enter Name Here'
+                    style={{width:"100%", height: "70px", textAlign:"center", border:"1px solid grey"}}/>
+    
+            <button className="btn btn-primary btn-block mx-auto Add-enter-buttons"
+                    style={{marginTop:"5%", width:"75%"}}>Add</button>
+          </form>
 
-                <button
-                    className="btn btn-secondary btn-block mx-auto Add-enter-buttons"
-                    style={{width:"75%", marginTop:"5%"}}
-                    onClick={() => this.props.onAddCrewmate()}
-                >
-                Cancel
-                </button>
-            </div>
-        );
+          <button className="btn btn-secondary btn-block mx-auto Add-enter-buttons"
+                  style={{width:"75%", marginTop:"5%"}}
+                  onClick={() => this.props.onAddCrewmate()}
+          >
+          Cancel
+          </button>
+        </React.Fragment>
+      );
     }
   }
 
