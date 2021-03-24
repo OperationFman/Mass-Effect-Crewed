b4144d0ebe52ad0ffa003d363daf8670866bb61c
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-24 14:51:05 +1100

    Styled back button

--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -36,7 +36,7 @@ class Overview extends Component {
                             />
             </div>
             <button type="button" 
-                    className="btn btn-primary btn-block mx-auto Main-buttons"
+                    className="btn btn-outline-dark btn-block Main-buttons"
                     style={{width:"100%"}}
                     onClick={this.refreshPage}>Back
             </button>
